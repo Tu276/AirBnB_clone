458cfea7b3cbb4e65711edec620460423d7ce044
Tu276 <66872848+Tu276@users.noreply.github.com>
2022-11-24 09:05:18 +0300

    Update generate-authors.sh
    generate autors.sh

--- generate-authors.sh
+++ generate-authors.sh
@@ -8,7 +8,7 @@ set -e
 {
 	cat <<- 'EOH'
 		# This file lists all individuals having contributed content to the repository.
-		# For how it is generated, see `hack/generate-authors.sh`.
+		# For how it is generated, see `generate-authors.sh`.
 	EOH
 	echo
 	git log --format='%aN <%aE>' | LC_ALL=C.UTF-8 sort -uf
