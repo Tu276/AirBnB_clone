fca205ef355f164602a88b5eaf6b9d4c8c9e475a
Gracy222 <gbamidele23@gmail.com>
2023-10-14 03:17:24 -0700

    updated model

--- models/base_model.py
+++ models/base_model.py
@@ -5,13 +5,22 @@ from datetime import datetime
 
 class BaseModel:
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
+        
+        if len(kwargs) != 0:
+            for key, value in kwargs.items():
+                if key ["__class__"]:
+                    continue;
+            if key == "updated_at" or key == "created_at":
+                self.__dict__[key] = datetime.fromisoformat(value);
+                    else:
+                        self.__dict__[key] = value;
+    else:
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
-        self.updated_at = datetime.now()
 
     def __str__(self):
-        return f"[{self __class__.__name__}] ({self.id}) {self.__dict__}"
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
         self.updated_at = datetime.now()
@@ -19,6 +28,6 @@ class BaseModel:
     def to_dict(self):
         dict_copy = self.__dict__.copy()
         dict_copy["__class__"] = self.__class__.__name__
-         dict_copy["created_at"] = self.created_at.isoformat()
-         dict_copy["updated_at"] = self.updated_at.isoformat()
-         return dict_copY
+        dict_copy["created_at"] = self.created_at.isoformat()
+        dict_copy["updated_at"] = self.updated_at.isoformat()
+        return dict_copy
