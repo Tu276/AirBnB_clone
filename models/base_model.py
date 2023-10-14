8e65ed40123e70afcc44d71d3abf240ac3007500
Gracy222 <gbamidele23@gmail.com>
2023-10-14 01:24:15 -0700

    model

--- models/base_model.py
+++ models/base_model.py
@@ -1,25 +1,24 @@
 #!/user/bin/python3
 
 import uuid
-from datetime import date
+from datetime import datetime
 
-class BaseModel():
-    def __init__ (self):
+class BaseModel:
+
+    def __init__(self):
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
-        self.updated_at = datetime.now
+        self.updated_at = datetime.now()
 
     def __str__(self):
-        return f"[{self__class__.__name__}] ({self.id}) {self.__dick__}"
-    
+        return f"[{self __class__.__name__}] ({self.id}) {self.__dict__}"
+
     def save(self):
-        self.updated_at =  datetime.now()
+        self.updated_at = datetime.now()
 
     def to_dict(self):
-        dick_copy = self.__dick__.copy()
-        dick_copy["__clas__"] = self.__class__.__name__
-        dick_copy["created_at"] = self.created_at.isoformat()
-        dick_copy["updated_at"] = self.updated_at.isoformat()
-        return dick_copy
-
-
+        dict_copy = self.__dict__.copy()
+        dict_copy["__class__"] = self.__class__.__name__
+         dict_copy["created_at"] = self.created_at.isoformat()
+         dict_copy["updated_at"] = self.updated_at.isoformat()
+         return dict_copY
